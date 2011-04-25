6e1950991e3275074bba266f8150554e2aa1ce44
Joaquín Magneres <joacomag@gmail.com>
2011-04-25 02:49:19 +0000

    Agrego nuevos estilos. Aplico el estilo al header

--- bibliotecaGrails/grails-app/conf/BootStrap.groovy
+++ bibliotecaGrails/grails-app/conf/BootStrap.groovy
@@ -1,31 +1,44 @@
 import grails.plugin.mail.MailService;
 import grails.util.GrailsUtil
+import biblioteca.Usuario
 
 class BootStrap {
-	def emailConfirmationService
+    
+    def emailConfirmationService
 
     def init = { servletContext ->
 		
-		emailConfirmationService.onConfirmation = { email, uid ->
-			def usuario = biblioteca.Usuario.findAllByEmail(email)
-			usuario.each { 
-				it.confirmado = true
-				it.save()
-			}
-			return [controller:'usuario', action:'confirmado']
-		}
+        emailConfirmationService.onConfirmation = { email, uid ->
+            def usuario = biblioteca.Usuario.findAllByEmail(email)
+            usuario.each {
+                it.confirmado = true
+                it.save()
+            }
+            return [controller:'usuario', action:'confirmado']
+        }
 		
-		emailConfirmationService.onInvalid = { uid -> 
-			def usuario = biblioteca.Usuario.findAllByEmail(email)
-			usuario.delete();
-			return [controller:'usuario', action:'confirmado']
-		}
+        emailConfirmationService.onInvalid = { uid ->
+            def usuario = biblioteca.Usuario.findAllByEmail(email)
+            usuario.delete();
+            return [controller:'usuario', action:'confirmado']
+        }
 		
-		emailConfirmationService.onTimeout = { email, uid -> 
-			def usuario = biblioteca.Usuario.findAllByEmail(email)
-			usuario.delete();
-			return [controller:'usuario', action:'confirmado']
-		}
-	}
+        emailConfirmationService.onTimeout = { email, uid ->
+            def usuario = biblioteca.Usuario.findAllByEmail(email)
+            usuario.delete();
+            return [controller:'usuario', action:'confirmado']
+        }
+
+        def admin = new Usuario(
+            login: "groso",
+            password: "groso",
+            email: "joacomag@gmail.com",
+            rol: "admin",
+        )
+        admin.save(flush:true)
+        admin.confirmado = true
+        admin.save(flush:true)
+               
+    }
 
 }
