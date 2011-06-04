4f014400a2fc1f32bbaf12b0120334b33138b6fb
Joaquín Magneres <joacomag@gmail.com>
2011-06-04 14:59:49 +0000

    Modifico la puntuacion con intencion de crear un DSL

--- bibliotecaGrails/grails-app/domain/biblioteca/Puntuacion.groovy
+++ bibliotecaGrails/grails-app/domain/biblioteca/Puntuacion.groovy
@@ -2,11 +2,29 @@ package biblioteca
 
 class Puntuacion {
 
-	Integer puntaje
-	
-	static belongsTo = [material:Material, autor:Usuario]
+    Integer puntaje
+    String unit
+    
+    static belongsTo = [material:Material, autor:Usuario]
 
+    def por(Usuario autor) {
+        this.autor = autor
+    }
+    
+    def de(Material material) {
+        this.material = material
+    }
+    
     static constraints = {
-		puntaje(range:0..5)
+        puntaje(range:0..5)
+    }
+
+}
+
+// Hago este DSL solo con fines demostrativos :P
+@Category(Number)
+class PuntuacionNumerica {
+    def get(String unit) {
+        new Puntuacion(this, unit, null, null)
     }
 }
